621a5fc31bd33cf53538fc43be3d3f18d2c19a39
neogulcity <neogulcity@gmail.com>
2021-09-09 18:33:27 +0900

    Add files via upload

---
+++ extern/CommonLibSSE/include/SKSE/Logger.h
@@ -0,0 +1,48 @@
+#pragma once
+
+#include "RE/B/BSTEvent.h"
+#include "RE/L/LogEvent.h"
+
+#define SKSE_MAKE_SOURCE_LOGGER(a_func, a_type)                           \
+                                                                          \
+	template <class... Args>                                              \
+	struct [[maybe_unused]] a_func                                        \
+	{                                                                     \
+		a_func() = delete;                                                \
+                                                                          \
+		template <class T>                                                \
+		explicit a_func(                                                  \
+			T&& a_fmt,                                                    \
+			Args&&... a_args,                                             \
+			std::source_location a_loc = std::source_location::current()) \
+		{                                                                 \
+			spdlog::log(                                                  \
+				spdlog::source_loc{                                       \
+					a_loc.file_name(),                                    \
+					static_cast<int>(a_loc.line()),                       \
+					a_loc.function_name() },                              \
+				spdlog::level::a_type,                                    \
+				std::forward<T>(a_fmt),                                   \
+				std::forward<Args>(a_args)...);                           \
+		}                                                                 \
+	};                                                                    \
+                                                                          \
+	template <class T, class... Args>                                     \
+	a_func(T&&, Args&&...) -> a_func<Args...>;
+
+namespace SKSE::log
+{
+	SKSE_MAKE_SOURCE_LOGGER(trace, trace);
+	SKSE_MAKE_SOURCE_LOGGER(debug, debug);
+	SKSE_MAKE_SOURCE_LOGGER(info, info);
+	SKSE_MAKE_SOURCE_LOGGER(warn, warn);
+	SKSE_MAKE_SOURCE_LOGGER(error, err);
+	SKSE_MAKE_SOURCE_LOGGER(critical, critical);
+
+	[[nodiscard]] std::optional<std::filesystem::path> log_directory();
+
+	void add_papyrus_sink(std::regex a_filter);
+	void remove_papyrus_sink();
+}
+
+#undef SKSE_MAKE_SOURCE_LOGGER
